1d5ab931fbb249d039aa4a40189179ae3ae44010
jrKitt <kittichdev@gmail.com>
2025-01-24 13:23:53 +0700

    Lab07

--- Lab07/src/T9Main.java
+++ Lab07/src/T9Main.java
@@ -9,9 +9,9 @@ public class T9Main {
         box.add(new Circle(2.5));
         box.add(new Circle(1.5));
         box.add(new Circle(1.0));
-
-        box.add(new Rectangle(6.0, 2.0));
-        box.add(new Triangle(6.0, 2.0));
+        
+//        box.add(new Rectangle(6.0, 2.0));
+//        box.add(new Triangle(6.0, 2.0));
 
         System.out.printf("Total area\t\t %.3f\n" ,box.getTotalArea());
         System.out.printf("Total circumference\t %.3f" ,box.getTotalCircumference());
