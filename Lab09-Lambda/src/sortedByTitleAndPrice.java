73fd7602a3f4eabace0d8575ef4fbeb5400e5cc2
Kittichai Raksawong <kittichdev@gmail.com>
2025-02-07 12:04:32 +0700

    Delete Lab09-Lambda/src/sortedByTitleAndPrice.java

--- Lab09-Lambda/src/sortedByTitleAndPrice.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author นายกิตติชัย รักษาวงค์ 673380028-2 Section.1
- */
-import java.util.Comparator;
-public class sortedByTitleAndPrice implements BookComparator {
-    public int compare(Book b1, Book b2) {
-        return Comparator.comparing(Book::getTitle).thenComparing(Book::getPrice).compare(b1, b2);
-    }
-}
