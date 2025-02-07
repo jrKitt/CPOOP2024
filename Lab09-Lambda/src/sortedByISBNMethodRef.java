867bcef53d6437b292e1b28b3d084e9cfa07fb5c
Kittichai Raksawong <kittichdev@gmail.com>
2025-02-07 12:04:40 +0700

    Delete Lab09-Lambda/src/sortedByISBNMethodRef.java

--- Lab09-Lambda/src/sortedByISBNMethodRef.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author นายกิตติชัย รักษาวงค์ 673380028-2 Section.1
- */
-import java.util.Comparator;
-public class sortedByISBNMethodRef implements BookComparator {
-    public int compare(Book b1, Book b2) {
-        return Comparator.comparing(Book::getIsbn).compare(b1, b2);
-    }
-}
